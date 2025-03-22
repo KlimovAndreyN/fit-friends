0423af61f97df34d35d9e44cf2128482ee6cdcdc
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-03-22 19:11:45 +0700

    удалили account/fit-questionnaire

--- backend/apps/account/src/app/app.module.ts
+++ backend/apps/account/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { getMongooseOptions } from '@backend/shared/helpers';
 import { AuthenticationModule } from '@backend/account/authentication'
-import { FitQuestionnaireModule } from '@backend/account/fit-questionnaire'
 import { AccountConfigModule } from '@backend/account/config'
 
 @Module({
   imports: [
     AuthenticationModule,
-    FitQuestionnaireModule,
     AccountConfigModule,
     MongooseModule.forRootAsync(getMongooseOptions())
   ],
--- backend/libs/account/fit-questionnaire/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { FitQuestionnaireModule } from './fit-questionnaire.module';
-
-export { FitQuestionnaireEntity } from './fit-questionnaire.entity';
-
-export { FitQuestionnaireRepository } from './fit-questionnaire.repository';
