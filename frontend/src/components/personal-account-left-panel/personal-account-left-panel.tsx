f511300528c8af406b817c26b118c6c8ff9b6a2a
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-03-24 21:34:05 +0700

    выделил PersonalAccountLeftPanel из PersonalAccount

---
+++ frontend/src/components/personal-account-left-panel/personal-account-left-panel.tsx
@@ -0,0 +1,102 @@
+import { Fragment, useState } from 'react';
+
+import AvatarUpload from '../../components/avatar-upload/avatar-upload';
+import PersonalAccountBlock from '../../components/personal-account-block/personal-account-block';
+import CustomInput from '../../components/custom-input/custom-input';
+import CustomSelect from '../../components/custom-select/custom-select';
+import SpecializationsCheckbox from '../../components/specializations-checkbox/specializations-checkbox';
+
+import { UserRole } from '@backend/shared';
+
+import { useAppSelector } from '../../hooks';
+import { getUserInfo, getUserRole } from '../../store/user-process/selectors';
+import { LOCATIONS, USER_GENDERS, USER_LEVELS } from '../../const';
+
+function PersonalAccountLeftPanel(): JSX.Element {
+  const [isEditing/*, setIsEditing*/] = useState(false);
+  const userRole = useAppSelector(getUserRole);
+  const userInfo = useAppSelector(getUserInfo);
+  const { name, avatarPath, about, specializations, metroStationName, gender, level } = userInfo;
+
+  //! отладка
+  // eslint-disable-next-line no-console
+  console.log('PersonalAccountLeftPanel', 'userRole,', userRole, 'userInfo', userInfo);
+
+  const mainClassName = `user-info${(isEditing) ? '-edit' : ''}`;
+  const buttonCaption = (isEditing) ? 'Сохранить' : 'Редактировать';
+  const isSpotsmanRole = (userRole === UserRole.Sportsman);
+  const readyForTrainingCaption = (isSpotsmanRole) ? 'Готов к тренировке' : 'Готов тренировать';
+
+  return (
+
+    <section className={mainClassName}>
+      <div className={`${mainClassName}__header`}>
+        {/* //! нужен будет обработчик при изменнии файла в режиме редактирования */}
+        <AvatarUpload name='user-photo-1' path={avatarPath} forPersonalAccount isShowButtons={isEditing} readonly={!isEditing} />
+      </div>
+      {/* //! нужен будет обработчик при сохранении формы, если редактирование */}
+      <form className={`${mainClassName}__form`} action="#" method="post">
+        {/* //! нужен будет обработчик при нажатии на кнопку, если просмотр */}
+        <button
+          className={`btn-flat btn-flat--underlined ${mainClassName}__${(isEditing) ? 'save' : 'edit'}-button`}
+          type={(isEditing) ? 'submit' : 'button'}
+          aria-label={buttonCaption}
+        >
+          <svg width="12" height="12" aria-hidden="true">
+            <use xlinkHref="#icon-edit"></use>
+          </svg>
+          <span>{buttonCaption}</span>
+        </button>
+        <PersonalAccountBlock mainClassNamePrefix={mainClassName} title='Обо мне' >
+          <Fragment>
+            <CustomInput type='text' name='name' label='Имя' value={name} divExtraClassName={`${mainClassName}__input`} readonly={!isEditing} />
+            <CustomInput type='textarea' name='description' label='Описание' value={about} divExtraClassName={`${mainClassName}__textarea`} readonly={!isEditing} />
+          </Fragment>
+        </PersonalAccountBlock>
+        <PersonalAccountBlock mainClassNamePrefix={mainClassName} extraClassNamePrefix='status' title='Статус' >
+          <div className={`custom-toggle custom-toggle--switch ${mainClassName}__toggle`}>
+            <label>
+              <input type="checkbox" name="ready-for-training" defaultChecked />
+              <span className="custom-toggle__icon">
+                <svg width="9" height="6" aria-hidden="true">
+                  <use xlinkHref="#arrow-check"></use>
+                </svg>
+              </span>
+              <span className="custom-toggle__label">{readyForTrainingCaption}</span>
+            </label>
+          </div>
+        </PersonalAccountBlock>
+        <PersonalAccountBlock mainClassNamePrefix={mainClassName} extraClassNamePrefix='specialization' title='Специализация' >
+          <SpecializationsCheckbox name='specialization' values={specializations} divExtraClassName={`${mainClassName}__specialization`} readonly={!isEditing} />
+        </PersonalAccountBlock>
+        <CustomSelect //! в макете к названию станции добавлено "ст. м. ", добавил titlePrefix
+          name='location'
+          caption='Локация'
+          value={metroStationName}
+          titlePrefix='ст. м. '
+          options={LOCATIONS}
+          extraClassName={`${mainClassName}__select`}
+          readonly={!isEditing}
+        />
+        <CustomSelect
+          name='sex'
+          caption='Пол'
+          value={gender}
+          options={USER_GENDERS}
+          extraClassName={`${mainClassName}__select`}
+          readonly={!isEditing}
+        />
+        <CustomSelect
+          name='level'
+          caption='Уровень'
+          value={level}
+          options={USER_LEVELS}
+          extraClassName={`${mainClassName}__select`}
+          readonly={!isEditing}
+        />
+      </form>
+    </section>
+  );
+}
+
+export default PersonalAccountLeftPanel;
--- frontend/src/pages/personal-account/personal-account.tsx
+++ frontend/src/pages/personal-account/personal-account.tsx
@@ -1,27 +1,22 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect } from 'react';
 
 import MainSpinner from '../../components/main-spinner/main-spinner';
 import Header from '../../components/header/header';
-import AvatarUpload from '../../components/avatar-upload/avatar-upload';
-import PersonalAccountBlock from '../../components/personal-account-block/personal-account-block';
-import CustomInput from '../../components/custom-input/custom-input';
-import CustomSelect from '../../components/custom-select/custom-select';
-import SpecializationsCheckbox from '../../components/specializations-checkbox/specializations-checkbox';
+import PersonalAccountLeftPanel from '../../components/personal-account-left-panel/personal-account-left-panel';
 
 import { UserRole } from '@backend/shared';
 
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { getIsFetchUserInfoExecuting, getUserInfo, getUserRole } from '../../store/user-process/selectors';
 import { fetchUserInfo } from '../../store/user-action';
-import { LOCATIONS, PageTitle, USER_GENDERS, USER_LEVELS } from '../../const';
+import { PageTitle } from '../../const';
 
 function PersonalAccount(): JSX.Element {
   const dispatch = useAppDispatch();
-  const [isEditing/*, setIsEditing*/] = useState(false);
   const isFetchUserInfoExecuting = useAppSelector(getIsFetchUserInfoExecuting);
   const userRole = useAppSelector(getUserRole);
   const userInfo = useAppSelector(getUserInfo);
-  const { name, avatarPath, about, specializations, metroStationName, gender, level } = userInfo;
+  const { caloriesWaste } = userInfo;
 
   useEffect(
     () => {
@@ -38,12 +33,9 @@ function PersonalAccount(): JSX.Element {
 
   //! отладка
   // eslint-disable-next-line no-console
-  console.log(userInfo);
+  console.log('PersonalAccountLeftPanel', 'userRole,', userRole, 'userInfo', userInfo);
 
-  const mainClassName = `user-info${(isEditing) ? '-edit' : ''}`;
-  const buttonCaption = (isEditing) ? 'Сохранить' : 'Редактировать';
   const isSpotsmanRole = (userRole === UserRole.Sportsman);
-  const readyForTrainingCaption = (isSpotsmanRole) ? 'Готов к тренировке' : 'Готов тренировать';
 
   return (
     <Fragment>
@@ -53,73 +45,7 @@ function PersonalAccount(): JSX.Element {
           <div className="container">
             <div className="inner-page__wrapper">
               <h1 className="visually-hidden">Личный кабинет</h1>
-              <section className={mainClassName}>
-                <div className={`${mainClassName}__header`}>
-                  {/* //! нужен будет обработчик при изменнии файла в режиме редактирования */}
-                  <AvatarUpload name='user-photo-1' path={avatarPath} forPersonalAccount isShowButtons={isEditing} readonly={!isEditing} />
-                </div>
-                {/* //! нужен будет обработчик при сохранении формы, если редактирование */}
-                <form className={`${mainClassName}__form`} action="#" method="post">
-                  {/* //! нужен будет обработчик при нажатии на кнопку, если просмотр */}
-                  <button
-                    className={`btn-flat btn-flat--underlined ${mainClassName}__${(isEditing) ? 'save' : 'edit'}-button`}
-                    type={(isEditing) ? 'submit' : 'button'}
-                    aria-label={buttonCaption}
-                  >
-                    <svg width="12" height="12" aria-hidden="true">
-                      <use xlinkHref="#icon-edit"></use>
-                    </svg>
-                    <span>{buttonCaption}</span>
-                  </button>
-                  <PersonalAccountBlock mainClassNamePrefix={mainClassName} title='Обо мне' >
-                    <Fragment>
-                      <CustomInput type='text' name='name' label='Имя' value={name} divExtraClassName={`${mainClassName}__input`} readonly={!isEditing} />
-                      <CustomInput type='textarea' name='description' label='Описание' value={about} divExtraClassName={`${mainClassName}__textarea`} readonly={!isEditing} />
-                    </Fragment>
-                  </PersonalAccountBlock>
-                  <PersonalAccountBlock mainClassNamePrefix={mainClassName} extraClassNamePrefix='status' title='Статус' >
-                    <div className={`custom-toggle custom-toggle--switch ${mainClassName}__toggle`}>
-                      <label>
-                        <input type="checkbox" name="ready-for-training" defaultChecked />
-                        <span className="custom-toggle__icon">
-                          <svg width="9" height="6" aria-hidden="true">
-                            <use xlinkHref="#arrow-check"></use>
-                          </svg>
-                        </span>
-                        <span className="custom-toggle__label">{readyForTrainingCaption}</span>
-                      </label>
-                    </div>
-                  </PersonalAccountBlock>
-                  <PersonalAccountBlock mainClassNamePrefix={mainClassName} extraClassNamePrefix='specialization' title='Специализация' >
-                    <SpecializationsCheckbox name='specialization' values={specializations} divExtraClassName={`${mainClassName}__specialization`} readonly={!isEditing} />
-                  </PersonalAccountBlock>
-                  <CustomSelect //! в макете к названию станции добавлено "ст. м. ", добавил titlePrefix
-                    name='location'
-                    caption='Локация'
-                    value={metroStationName}
-                    titlePrefix='ст. м. '
-                    options={LOCATIONS}
-                    extraClassName={`${mainClassName}__select`}
-                    readonly={!isEditing}
-                  />
-                  <CustomSelect
-                    name='sex'
-                    caption='Пол'
-                    value={gender}
-                    options={USER_GENDERS}
-                    extraClassName={`${mainClassName}__select`}
-                    readonly={!isEditing}
-                  />
-                  <CustomSelect
-                    name='level'
-                    caption='Уровень'
-                    value={level}
-                    options={USER_LEVELS}
-                    extraClassName={`${mainClassName}__select`}
-                    readonly={!isEditing}
-                  />
-                </form>
-              </section>
+              <PersonalAccountLeftPanel />
               <div className="inner-page__content">
                 {
                   (isSpotsmanRole)
@@ -131,14 +57,17 @@ function PersonalAccount(): JSX.Element {
                             <div className="personal-account-user__input">
                               <label>
                                 <span className="personal-account-user__label">План на день, ккал</span>
-                                {/*//! или выключить или что по ТЗ, но тип поменять на number, как быть со вторым полем... */}
-                                <input type="text" name="schedule-for-the-day" defaultValue="3 300" />
+                                {/*
+                                //! или выключить или что по ТЗ можно их менять?, но тип поменять на number, как быть со вторым полем...
+                                //! когда будет тренер, то выделеить на разные под компоненты
+                                */}
+                                <input type="text" name="schedule-for-the-day" defaultValue={caloriesWaste} disabled />
                               </label>
                             </div>
                             <div className="personal-account-user__input">
                               <label>
                                 <span className="personal-account-user__label">План на неделю, ккал</span>
-                                <input type="text" name="schedule-for-the-week" defaultValue="23 100" />
+                                <input type="text" name="schedule-for-the-week" defaultValue={caloriesWaste || 0 * 7} disabled />
                               </label>
                             </div>
                           </div>
