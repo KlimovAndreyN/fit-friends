49e713ce692a24914dc88ff838eda2d541028443
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-05-27 14:47:00 +0700

    IUserProfilesWithPaginationRdo

--- backend/libs/shared/core/src/fronted-index.ts
+++ backend/libs/shared/core/src/fronted-index.ts
@@ -29,6 +29,7 @@ export { type ITrainingRdo } from './lib/interfaces/rdo/i-training.rdo';
 export { type ITrainingsWithPaginationRdo } from './lib/interfaces/rdo/i-trainings-with-pagination.rdo';
 export { type IUserProfileRdo } from './lib/interfaces/rdo/i-user-profile.rdo';
 export { type IUserRdo } from './lib/interfaces/rdo/i-user.rdo';
+export { type IUserProfilesWithPaginationRdo } from './lib/interfaces/rdo/i-user-profiles-with-pagination.rdo';
 
 export { type TokenPayload as ITokenPayloadRdo } from './lib/interfaces/token-payload.interface';
 export { type Tokens as ITokensRdo } from './lib/interfaces/tokens.interface';
---
+++ backend/libs/shared/core/src/lib/interfaces/rdo/i-user-profiles-with-pagination.rdo.ts
@@ -0,0 +1,4 @@
+import { PaginationResult } from '../pagination-result.interface';
+import { IUserProfileRdo } from './i-user-profile.rdo';
+
+export type IUserProfilesWithPaginationRdo = PaginationResult<IUserProfileRdo>;
