421c8b822a67bb8e5c13c857e3de36f4b002eea4
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-06-01 20:30:32 +0700

    докоммит

---
+++ frontend/src/components/user-detail-wrapper-content-head/user-detail-wrapper-content-head.tsx
@@ -0,0 +1,66 @@
+import { Fragment, JSX, MouseEvent, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { IDetailUserProfileRdo } from '@backend/shared/core';
+
+import UserPhoto from '../user-photo/user-photo';
+import UserDetailStatus from '../user-detail-status/user-detail-status';
+import PopupModalMap from '../popup-modal-map/popup-modal-map';
+
+import { LocationTitle } from '../../const';
+
+type UserDetailWrapperContentHeadProps = {
+  classNamePrefix: string;
+  detailUserProfile: IDetailUserProfileRdo;
+}
+
+function UserDetailWrapperContentHead({ classNamePrefix, detailUserProfile }: UserDetailWrapperContentHeadProps): JSX.Element {
+  //! возможно отдельный компонет Location, где иконка #icon-location
+  //    нет css для user-card__icon-location / user-card-coach__icon-location / user-card-coach-2__icon-location, но в svg выставлено
+  // добавил UserPhoto
+
+  const { pathname } = useLocation();
+  const {
+    user: { role, name, avatarFilePath, location, about },
+    questionnaire: { readyForTraining, description }
+  } = detailUserProfile;
+  const locationText = LocationTitle[location];
+  const [showLocationMap, setShowLocationMap] = useState(false);
+
+  const handleLocationLinkClick = (event: MouseEvent) => {
+    event.preventDefault();
+    setShowLocationMap(true);
+  };
+
+  const handleLocationPopupModalMapClose = () => {
+    setShowLocationMap(false);
+  };
+
+  return (
+    <Fragment>
+      <UserPhoto className={`${classNamePrefix}__head`} size={80} path={avatarFilePath} />
+      <div className={`${classNamePrefix}__head`}>
+        <h2 className={`${classNamePrefix}__title`}>{name}</h2>
+      </div>
+      <div className={`${classNamePrefix}__label`}>
+        <a href={`${pathname}#popup-user-map.html`} onClick={handleLocationLinkClick}>
+          <svg className={`${classNamePrefix}__icon-location`} width="12" height="14" aria-hidden="true">
+            <use xlinkHref="#icon-location"></use>
+          </svg>
+          <span>{locationText}</span>
+        </a>
+      </div>
+      {
+        showLocationMap &&
+        <PopupModalMap title={name} address={locationText} onClose={handleLocationPopupModalMapClose} />
+      }
+      <UserDetailStatus role={role} readyForTraining={readyForTraining} />
+      <div className={`${classNamePrefix}__text`}>
+        <p>{about}</p>
+        {(description) && <p>{description}</p>}
+      </div>
+    </Fragment>
+  );
+}
+
+export default UserDetailWrapperContentHead;
--- frontend/src/components/user-detail-wrapper-content/user-detail-wrapper-content.tsx
+++ frontend/src/components/user-detail-wrapper-content/user-detail-wrapper-content.tsx
@@ -1,16 +1,12 @@
-import { JSX, MouseEvent, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { JSX, MouseEvent } from 'react';
 
 import { IDetailUserProfileRdo, isCoachRole } from '@backend/shared/core';
 
-import UserPhoto from '../user-photo/user-photo';
 import Hashtags from '../hashtags/hashtags';
-import UserDetailStatus from '../user-detail-status/user-detail-status';
-import PopupModalMap from '../popup-modal-map/popup-modal-map';
+import UserDetailWrapperContentHead from '../user-detail-wrapper-content-head/user-detail-wrapper-content-head';
 import UserDetailWrapperContentCoachCertificates from '../user-detail-wrapper-content-coach-certificates copy/user-detail-wrapper-content-coach-certificates';
 
 import { getSpecializationsTitles } from '../../utils/common';
-import { LocationTitle } from '../../const';
 
 type UserDetailWrapperContentProps = {
   classNamePrefix: string;
@@ -19,30 +15,12 @@ type UserDetailWrapperContentProps = {
 
 function UserDetailWrapperContent({ classNamePrefix, detailUserProfile }: UserDetailWrapperContentProps): JSX.Element {
   //! кнопка - добавить в друзья
-  //! много кода, попробовать разделить, сертификаты... что то еще
 
-  //! возможно отдельный компонет Location, где иконка #icon-location
-  //    нет css для user-card__icon-location / user-card-coach__icon-location / user-card-coach-2__icon-location, но в svg выставлено
-  //! просмотр карты с местоположением - обработка клика в Location карта href={`${pathname}#popup-user-map.html`}
-  // добавил UserPhoto
-
-  const { pathname } = useLocation();
   const {
-    user: { role, name, avatarFilePath, location, about },
-    questionnaire: { readyForTraining, specializations, description, certificates = [] }
+    user: { role },
+    questionnaire: { specializations, certificates = [] }
   } = detailUserProfile;
-  const locationText = LocationTitle[location];
-  const [showLocationMap, setShowLocationMap] = useState(false);
   const isCoach = isCoachRole(role);
-
-  const handleLocationLinkClick = (event: MouseEvent) => {
-    event.preventDefault();
-    setShowLocationMap(true);
-  };
-
-  const handleLocationPopupModalMapClose = () => {
-    setShowLocationMap(false);
-  };
 
   const handleAddFriendButtonClick = (event: MouseEvent) => {
     event.preventDefault();
@@ -54,27 +32,7 @@ function UserDetailWrapperContent({ classNamePrefix, detailUserProfile }: UserDe
 
   return (
     <div className={`${classNamePrefix}__content`}>
-      <UserPhoto className={`${classNamePrefix}__head`} size={80} path={avatarFilePath} />
-      <div className={`${classNamePrefix}__head`}>
-        <h2 className={`${classNamePrefix}__title`}>{name}</h2>
-      </div>
-      <div className={`${classNamePrefix}__label`}>
-        <a href={`${pathname}#popup-user-map.html`} onClick={handleLocationLinkClick}>
-          <svg className={`${classNamePrefix}__icon-location`} width="12" height="14" aria-hidden="true">
-            <use xlinkHref="#icon-location"></use>
-          </svg>
-          <span>{locationText}</span>
-        </a>
-      </div>
-      {
-        showLocationMap &&
-        <PopupModalMap title={name} address={locationText} onClose={handleLocationPopupModalMapClose} />
-      }
-      <UserDetailStatus role={role} readyForTraining={readyForTraining} />
-      <div className={`${classNamePrefix}__text`}>
-        <p>{about}</p>
-        {(description) && <p>{description}</p>}
-      </div>
+      <UserDetailWrapperContentHead classNamePrefix={classNamePrefix} detailUserProfile={detailUserProfile} />
       {
         isCoach &&
         <UserDetailWrapperContentCoachCertificates classNamePrefix={classNamePrefix} certificates={certificates} />
