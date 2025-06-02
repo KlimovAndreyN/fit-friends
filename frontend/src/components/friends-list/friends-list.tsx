cf221328679860e41423df7b611f7a51bf435582
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-06-02 12:33:21 +0700

    поменял разметку для компонентов

--- frontend/src/components/friends-list/friends-list.tsx
+++ frontend/src/components/friends-list/friends-list.tsx
@@ -1,6 +1,5 @@
-import { JSX } from 'react';
+import { Fragment, JSX } from 'react';
 
-import BackButton from '../back-button/back-button';
 import ButtonsShowMoreAndToTop from '../buttons-show-more-and-to-top/buttons-show-more-and-to-top';
 import ThumbnailFriend from '../thumbnail-friend/thumbnail-friend';
 
@@ -8,7 +7,12 @@ import { useAppSelector } from '../../hooks';
 import { getUserRole } from '../../store/user-process/selectors';
 import { MOCK_FRIENDS } from '../../const';
 
-function FriendsList(): JSX.Element {
+
+type FriendsListProps = {
+  className: string;
+}
+
+function FriendsList({ className }: FriendsListProps): JSX.Element {
   //! проверить консоль браузера на ошибки
   //! вызвать в useEffect dispatch clearDetailUserProfile + setPrevLocation
   //! если нет друзей - вывести текст 'У вас еще нет друзей'
@@ -17,7 +21,6 @@ function FriendsList(): JSX.Element {
   const friends = MOCK_FRIENDS;
   const isHaveMoreData = true;
   //const isHaveMoreData = false;
-  const mainClassName = 'friends-list';
 
   const handleShowMoreClick = () => {
     // eslint-disable-next-line no-console
@@ -25,33 +28,24 @@ function FriendsList(): JSX.Element {
   };
 
   return (
-    <main>
-      <section className={mainClassName}>
-        <div className="container">
-          <div className={`${mainClassName}__wrapper`}>
-            <BackButton className={mainClassName} />
-            <div className={`${mainClassName}__title-wrapper`}>
-              <h1 className={`${mainClassName}__title`}>Мои друзья</h1>
-              {/* //! закоментировано в маркапах? есть компонет CustomCheckbox
-                <!--<div className="custom-toggle custom-toggle--switch custom-toggle--switch-right" data-validate-type="checkbox"><label><input type="checkbox" value="user-agreement-1" name="user-agreement"><span className="custom-toggle__icon">
-                      <svg width="9" height="6" aria-hidden="true"><use xlinkHref="#arrow-check"/></svg></span><span className="custom-toggle__label">Только онлайн</span></label></div>--> */}
-            </div>
-            <ul className={`${mainClassName}__list`}>
-              {
-                friends.map((friend) => (
-                  <ThumbnailFriend
-                    key={friend.id}
-                    className={mainClassName}
-                    friend={friend}
-                    userRole={userRole}
-                  />))
-              }
-            </ul>
-            <ButtonsShowMoreAndToTop divClassNamePrefix={mainClassName} isHaveMoreData={isHaveMoreData} onShowMoreClick={handleShowMoreClick} />
-          </div>
-        </div>
-      </section>
-    </main>
+    <Fragment>
+      <ul className={`${className}__list`}>
+        {
+          friends.map((friend) => (
+            <ThumbnailFriend
+              key={friend.id}
+              className={className}
+              friend={friend}
+              userRole={userRole}
+            />))
+        }
+      </ul>
+      <ButtonsShowMoreAndToTop
+        divClassNamePrefix={className}
+        isHaveMoreData={isHaveMoreData}
+        onShowMoreClick={handleShowMoreClick}
+      />
+    </Fragment>
   );
 }
 
--- frontend/src/pages/friends/friends.tsx
+++ frontend/src/pages/friends/friends.tsx
@@ -2,14 +2,32 @@ import { JSX, Fragment } from 'react';
 
 import Header from '../../components/header/header';
 import FriendsList from '../../components/friends-list/friends-list';
+import BackButton from '../../components/back-button/back-button';
 
 import { PageTitle } from '../../const';
 
 function Friends(): JSX.Element {
+  const mainClassName = 'friends-list';
+
   return (
     <Fragment>
       <Header title={PageTitle.Friends} />
-      <FriendsList />
+      <main>
+        <section className={mainClassName}>
+          <div className="container">
+            <div className={`${mainClassName}__wrapper`}>
+              <BackButton className={mainClassName} />
+              <div className={`${mainClassName}__title-wrapper`}>
+                <h1 className={`${mainClassName}__title`}>Мои друзья</h1>
+                {/* //! закоментировано в маркапах? есть компонет CustomCheckbox
+                <!--<div className="custom-toggle custom-toggle--switch custom-toggle--switch-right" data-validate-type="checkbox"><label><input type="checkbox" value="user-agreement-1" name="user-agreement"><span className="custom-toggle__icon">
+                      <svg width="9" height="6" aria-hidden="true"><use xlinkHref="#arrow-check"/></svg></span><span className="custom-toggle__label">Только онлайн</span></label></div>--> */}
+              </div>
+              <FriendsList className={mainClassName} />
+            </div>
+          </div>
+        </section>
+      </main>
     </Fragment>
   );
 }
